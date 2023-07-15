1bea5fb974e54b7f5090eeaddff6f9ac6076ec46
GuGuskyastro <95249732+GuGuskyastro@users.noreply.github.com>
2023-07-15 16:46:35 +0200

    Update api_connector.py

--- backend/api_connector.py
+++ backend/api_connector.py
@@ -9,16 +9,13 @@ class APIConnector:
     def __init__(self):
         # OpenAI API key
         os.environ['OPENAI_API_KEY'] = '${{ secrets.OPENAI_API_KEY }}'
-        weaviate_url = os.environ.get('WEAVIATE_URL')
-        weaviate_api_key = os.environ.get('WEAVIATE_API_KEY')
-        huggingface_api_key = os.environ.get('HUGGINGFACE_API_KEY')
 
         # Weaviate client
         self.client = weaviate.Client(
-            url=weaviate_url,
-            auth_client_secret=weaviate.AuthApiKey(api_key=weaviate_api_key),
+            url=os.environ['WEAVIATE_URL'],
+            auth_client_secret=weaviate.AuthApiKey(api_key=os.environ['WEAVIATE_AUTH_KEY']),
             additional_headers={
-                "X-HuggingFace-Api-Key": huggingface_api_key
+                "X-HuggingFace-Api-Key": os.environ['HUGGINGFACE_API_KEY']
             }
         )
 
