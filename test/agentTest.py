7423535569ed2f8f5fbc3abae4aed9bbce2768a3
GuGuskyastro <95249732+GuGuskyastro@users.noreply.github.com>
2023-07-17 01:42:31 +0200

    Update agentTest.py

--- test/agentTest.py
+++ test/agentTest.py
@@ -4,23 +4,39 @@ from backend.agent_utils import AgentUtils
 from backend.api_connector import APIConnector
 
 class AgentTest(unittest.TestCase):
+    """
+     Test if the functions used by the Agent tool and Agent are working correctly
+     
+    """
+    
     connector = APIConnector()
     utils = AgentUtils(llm=connector.llm, client=connector.client)
 
     def test_extraction(self):
+        """
+         Conference information should be extracted correctly.
+
+        """
         text = "Jonathan Berant, Andrew Chou, Roy Frostig, and Percy Liang. 2013. Semantic parsing on Freebase from question-answer pairs. In Proceedings of the 2013 Conference on Empirical Methods in Natural Language Processing, pages 1533–1544, Seattle, Washington, USA."
         result = AgentUtils.extraction(AgentTest.utils, text)
         self.assertIn('Conference short name: "EMNLP 2013"' , result)
 
 
     def test_weaviate(self):
+        """
+         Should judged that the conference is included in the weaviateVS. 
 
+        """  
         text = "Conference on Empirical Methods in Natural Language Processing 2013"
         result = AgentUtils.weaviate_query_input(AgentTest.utils, text)
         self.assertIn("The Conference on Empirical Methods in Natural Language Processing 2013 is stored in the Database", result)
 
 
     def test_run(self):
+        """
+         Agent should give the correct metadata for the conference mentioned in the text.
+
+        """  
         text = "Jonathan Berant, Andrew Chou, Roy Frostig, and Percy Liang. 2013. Semantic parsing on Freebase from question-answer pairs. In Proceedings of the 2013 Conference on Empirical Methods in Natural Language Processing, pages 1533–1544, Seattle, Washington, USA."
         result = run(text)
 
