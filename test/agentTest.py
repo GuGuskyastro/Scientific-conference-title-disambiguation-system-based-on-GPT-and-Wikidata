52416b919f0388613722f692fa40503d054a5e64
GuGuskyastro <95249732+GuGuskyastro@users.noreply.github.com>
2023-07-17 14:35:27 +0200

    Update agentTest.py

--- test/agentTest.py
+++ test/agentTest.py
@@ -41,7 +41,7 @@ class AgentTest(unittest.TestCase):
 
         """
         text = "Jonathan Berant, Andrew Chou, Roy Frostig, and Percy Liang. 2013. Semantic parsing on Freebase from question-answer pairs. In Proceedings of the 2013 Conference on Empirical Methods in Natural Language Processing, pages 1533–1544, Seattle, Washington, USA."
-        result = Agent.run2(AgentTest.agent,text)
+        result = Agent.run(AgentTest.agent,text)
 
         self.assertIn("Q109527338", result)
         self.assertIn("https://aclanthology.org/events/emnlp-2013", result)
