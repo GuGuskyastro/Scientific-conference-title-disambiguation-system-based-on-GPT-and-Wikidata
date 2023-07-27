39214f74c2891386934c51072f5c00d964a93c0f
GuGuskyastro <95249732+GuGuskyastro@users.noreply.github.com>
2023-07-27 17:27:40 +0200

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,8 @@ from backend.api_connector import APIConnector
 from backend.agent_utils import AgentUtils
 from backend.agent_build import AgentBuilder
 from langchain.agents import Tool
-
+import yaml
+from langchain.callbacks import get_openai_callback
 
 class Agent:
     """
@@ -28,56 +29,31 @@ class Agent:
         )
     ]
 
-    def run(self,text):
+    def run(self,text,show_token=True):
         """
          Agent process
 
          Args:
-             input (str): User input, which in the case of this agent should be the citations text of the paper.
+             text (str): User input, which in the case of this agent should be the citations text of the paper.
 
          Returns:
              str: complete analysis results including citation text and its corresponding conference information.
         """
 
-        template = """
-        You are now a text parsing assistant, and you receive a text of citations from a paper. Strictly follow the following workflow to complete the task:
-        For each citation in the input text,
-        1. Use the Extraction tool to parse the user input, that is, the user input text is used as the input of the tool. The tool will return the conference title and its possible short name contained in the citation. If the results of this step show that there is no information on any conference, skip the second step.
-        2. Use the Query tool to query conference title, that is, the title returned by Extraction tool is used as the input of query tool. 
-        
-        After operating all citations, summarize the results and give the final answer like:
-        There are a total of x citations in the input text (which is the user input), of which y contain scientific conference information. Following is extracted meeting information:
-        properties: {{
-                Citation text: {{"type": "string"}},
-                (Add following if there is one)
-                Conference title: {{"type": "string"}},
-                Conference short name: {{"type": "string"}},
-                Conference Qid: {{"type": "string"}},
-                Conference startDate: {{"type": "string"}},
-                Conference endDate: {{"type": "string"}},
-                Conference location: {{"type": "string"}}
-                Conference officialWebsite: {{"type": "string"}}
-                }}
-        
-        You have access to the following tools:
-        
-        {tools}
-        
-        Use the following format:
-        
-        Question: the input text from user
-        Thought: Think about what you need to do.
-        Action: the action to take, should be one of [{tool_names}]!
-        Action Input: the input to the tool. 
-        Observation: the result of the action
-        ... (this Thought/Action/Action Input/Observation can repeat N times)
-        
-        Thought: I now know the final answer
-        Final Answer: the final answer according to the previous tips
-        
-        Question: {input}
-        {agent_scratchpad}
-        """
+        with open('backend/templates.yaml', 'r', encoding='utf-8') as file:
+            templates = yaml.safe_load(file)
 
-        agent_executor = AgentBuilder.build_agent(tools=Agent.tools, template=template, llm=Agent.llm, verbose=True)
-        return agent_executor.run(text)
+        base_templates = templates['base_template']
+
+
+        agent_executor = AgentBuilder.build_agent(tools=Agent.tools, template=base_templates, llm=Agent.llm, verbose=True)
+
+        with get_openai_callback() as cb:
+            response = agent_executor.run(text)
+            total_tokens = f"Total Tokens: {cb.total_tokens}"
+            total_cost = f"Total Cost (USD): ${cb.total_cost}"
+
+        if show_token:
+            response += f"\n{total_tokens}\n{total_cost}"
+
+        return response
