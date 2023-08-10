76325861e7f26c08eab12736291ff27b6f7230a0
GuGuskyastro <95249732+GuGuskyastro@users.noreply.github.com>
2023-08-10 19:40:19 +0200

    Add files via upload

--- accuracyTest/acc_test.py
+++ accuracyTest/acc_test.py
@@ -3,10 +3,19 @@ import time
 import ruamel.yaml as yaml
 from backend.main import Agent
 
-def run_tests():
+def run_tests(output_file,model_name,use_integrate_agent):
+    """
+     Test the selected Agent model and method.
+
+     Args:
+         output_file (str): output filename
+         model_name (str): the model name of OpenAI,  can choose gpt-4 and gpt-3.5-turbo.
+         use_integrate_agent (bool): deciding which Agent method to use
+
+    """
+
     test_data_file = os.path.join(os.path.dirname(__file__), 'testText.yaml').replace("\\", "/")
-    output_file = "test_output.yaml"
-    agent = Agent()
+    agent = Agent(model_name=model_name)
 
     with open(test_data_file, "r", encoding="utf-8") as test_data:
         test_data = yaml.load(test_data)
@@ -18,7 +27,7 @@ def run_tests():
         text = entry['text']
 
         start_time = time.time()
-        output = Agent.run(agent, text, show_token=True)
+        output = Agent.run(agent, text, show_token=True,use_integrate_agent = use_integrate_agent)
         end_time = time.time()
 
         results[name] = {
@@ -29,5 +38,13 @@ def run_tests():
     with open(output_file, "w", encoding="utf-8") as output_file:
         yaml.dump(results, output_file, default_style="|", default_flow_style=False, encoding="utf-8")
 
-if __name__ == "__main__":
-    run_tests()
+if __name__ == '__main__':
+    run_tests('test_output_individually.yaml', 'gpt-3.5-turbo', False)
+    run_tests('test_output_integrate.yaml','gpt-3.5-turbo',True)
+    run_tests('test_output_integrate_gpt4.yaml', 'gpt-4', True)
+
+
+
+
+
+
